4b0410325d3bcdb5da9da580c144ff642066bf26
Nattell <midulcecaleb@gmail.com>
2024-12-18 17:37:55 +0100

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,37 @@
+# "3+4"  ---> 7
+
+# 1. Recoger datos del usuario
+primer_numero = input("primer numero:")
+operacion = input("operacion:")
+segundo_numero = input("segundo numero:")
+
+# 1.5 Convertir las str en 2 int
+primer_numero = int(primer_numero)
+segundo_numero = int(segundo_numero)
+
+# 2. Calcular el resultado
+
+match operacion:
+    case "+":
+        print(primer_numero + segundo_numero)
+    case "-":
+        print(primer_numero - segundo_numero)
+    case "*":
+        print(primer_numero * segundo_numero)
+    case "/":
+        print(primer_numero / segundo_numero)
+    case _:
+        print("ninguna operacion es viable")
+        exit()
+
+if operacion == "+":
+    print(primer_numero + segundo_numero)
+elif operacion == "-":
+    print(primer_numero - segundo_numero)
+elif operacion == "*":
+    print(primer_numero * segundo_numero)
+elif operacion == "/":
+    print(primer_numero / segundo_numero)
+else:
+    print("ninguna operacion es viable")
+    exit()
